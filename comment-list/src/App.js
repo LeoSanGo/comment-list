3436e7f53df339e69d6823bd027134d26eefbb76
leosango <souoleo@gmail.com>
2022-03-03 17:21:33 -0300

    states to hold comments

--- comment-list/src/App.js
+++ comment-list/src/App.js
@@ -1,34 +1,28 @@
 import './App.css';
-import React, {useState} from 'react'
-
+import React, { useState } from 'react';
 
 function App() {
-
-  const [comment, setComments] = useState()
-  const [allComments, setAllComments] = useState([])
+  const [comment, setComments] = useState();
+  const [allComments, setAllComments] = useState([]);
 
   const handleTextArea = (event) => {
     setComments(event.target.value);
-  }
+  };
 
   const makeComment = () => {
-    const receiveAllComments = [...allComments, comment]
-    setAllComments(receiveAllComments)
-  }
+    const receiveAllComments = [...allComments, comment];
+    setAllComments(receiveAllComments);
+  };
   return (
-    <div >
-      
+    <div>
       <h1>TESTE</h1>
-      <textarea onChange={handleTextArea}/>
+      <textarea onChange={handleTextArea} />
       <button onClick={makeComment}>Comentar</button>
 
       <ul>
-        {
-          allComments.map((oneComment, index) => (
-            <li key={index}>{oneComment}</li>
-          ))
-        }
-        
+        {allComments.map((oneComment, index) => (
+          <li key={index}>{oneComment}</li>
+        ))}
       </ul>
     </div>
   );
