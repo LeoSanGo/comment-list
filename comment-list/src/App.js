403ee3edc0f7404a31af190931d0458d78d9bdaf
leosango <souoleo@gmail.com>
2022-03-03 17:14:50 -0300

    Initial project

---
+++ comment-list/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import React, {useState} from 'react'
+
+
+function App() {
+
+  const [comment, setComments] = useState()
+  const [allComments, setAllComments] = useState([])
+
+  const handleTextArea = (event) => {
+    setComments(event.target.value);
+  }
+
+  const makeComment = () => {
+    const receiveAllComments = [...allComments, comment]
+    setAllComments(receiveAllComments)
+  }
+  return (
+    <div >
+      
+      <h1>TESTE</h1>
+      <textarea onChange={handleTextArea}/>
+      <button onClick={makeComment}>Comentar</button>
+
+      <ul>
+        {
+          allComments.map((oneComment, index) => (
+            <li key={index}>{oneComment}</li>
+          ))
+        }
+        
+      </ul>
+    </div>
+  );
+}
+
+export default App;
